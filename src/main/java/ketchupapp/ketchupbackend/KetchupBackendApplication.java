f7155c89ee2e56e5e81041eaf5fecf2a2b225ec0
GuillermoSalgado1 <g.salgado04@ufromail.cl>
2025-10-08 18:18:37 -0300

    Initial commit

---
+++ src/main/java/ketchupapp/ketchupbackend/KetchupBackendApplication.java
@@ -0,0 +1,13 @@
+package ketchupapp.ketchupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KetchupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KetchupBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ketchupapp/ketchupbackend/KetchupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ketchupapp.ketchupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KetchupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
