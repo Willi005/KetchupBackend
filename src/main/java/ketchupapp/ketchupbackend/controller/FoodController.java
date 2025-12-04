9199a511d6840f15ee032d25d3d5f628e7018ca8
GuillermoSalgado1 <g.salgado04@ufromail.cl>
2025-12-04 12:59:54 -0300

    collection db fix

--- src/main/java/ketchupapp/ketchupbackend/controller/FoodController.java
+++ src/main/java/ketchupapp/ketchupbackend/controller/FoodController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/food")
+@RequestMapping("/foods")
 @CrossOrigin(origins="http://localhost:5173")
 public class FoodController {
 
